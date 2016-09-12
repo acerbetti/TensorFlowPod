8534955f01c269ff84490c0dced207274c473d9e
Stefano Acerbetti <sacerbetti@birst.com>
2016-09-12 16:59:44 -0700

    Created demo project

---
+++ TensorFlowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TensorFlowDemo
+//
+//  Created by Stefano Acerbetti on 9/12/16.
+//  Copyright © 2016 Aceland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
